f15e0b6c3d1c34df0e4331ae33d32d96806c482e
AbLyc <67259797+AbLyc@users.noreply.github.com>
2021-11-08 20:19:20 +0330

    Update Time.py

--- Time.py
+++ Time.py
@@ -11,8 +11,8 @@ import aiocron
 import asyncio
 os.system('pkg install python && pip install telethon && pip install asyncio && pip install aiocron && pip install pytz && clear') 
 
-api_id = "" 
-api_hash=""
+api_id = "1203921" 
+api_hash="07efbc59f267f50db081a5aff8211fca"
 
 
 
